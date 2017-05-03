4cda9cd7335a8ef6f3c34ed7cb7a9dc1aaef5297
Will Pride <wpride@dimagi.com>
2017-05-03 15:29:21 +0200

    fix tests

--- src/test/java/tests/CaseTilesTest.java
+++ src/test/java/tests/CaseTilesTest.java
@@ -1,25 +1,13 @@
 package tests;
 
-import auth.HqAuth;
-import beans.menus.BaseResponseBean;
 import beans.menus.CommandListResponseBean;
 import beans.menus.EntityListResponse;
 import beans.menus.Tile;
-import org.json.JSONObject;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import utils.FileUtils;
 import utils.TestContext;
-
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Arrays;
-
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyString;
-import static org.mockito.Mockito.when;
 
 /**
  * Created by willpride on 4/14/16.
@@ -43,7 +31,7 @@ public class CaseTilesTest extends BaseTestClass{
     public void testCaseTiles() throws Exception {
         EntityListResponse response =
                 sessionNavigate(new String[] {"2"}, "casetiles", EntityListResponse.class);
-        Tile[] tiles = response.getTiles();
+        Tile[] tiles = response.getCaseTileConfiguration().getTiles();
         assert tiles.length == 14;
         assert tiles[13].getFontSize().equals("small");
         assert tiles[0].getGridX() == 0;
@@ -57,7 +45,6 @@ public class CaseTilesTest extends BaseTestClass{
         CommandListResponseBean response =
                 sessionNavigate(new String[] {"2", "bf1fc10c-ec65-4af7-b2a4-aa38dcb7af0c"}, "casetiles", CommandListResponseBean.class);
         assert response.getCommands().length == 5;
-        //assert response.getPersistentCaseTile().isUsesCaseTiles();
-        assert response.getPersistentCaseTile().getTiles().length == 14;
+        assert response.getPersistentCaseTile().getCaseTileConfiguration().getTiles().length == 14;
     }
 }
